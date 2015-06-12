a836c9d8b1a9e4f0e8941d5a55c1ba3e1a16d586
Gwen-Pigat <gwenael.pigat@gmail.com>
2015-06-12 09:25:39 +0200

    Logout detruit session + redirige en index + ferme mysql

--- php/logout.php
+++ php/logout.php
@@ -1,1 +1,1 @@
-<?php session_start(); session_destroy(); header('Location: ../index.html'); mysql_close(); ?>
+<?php session_start(); session_destroy(); mysql_close(); header('Location: ../index.html'); ?>
